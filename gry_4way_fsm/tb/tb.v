54d198da2ce27adf2f55b456fa48f9843f8024e8
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-09-12 23:56:10 +0800

    Create tb.v

---
+++ gry_4way_fsm/tb/tb.v
@@ -0,0 +1,41 @@
+timescale lns/lps
+module tb;
+
+reg			clka;
+reg			rstn;
+
+initial
+begin
+	#0;
+	clka = 1'b0;
+	rstna = 1'b0;
+	#101;
+	rstna = 1'b1;
+end
+
+always
+begin
+	#10 clk = ~clk;
+end
+
+
+gry_4way_fsm gry_4way_fsm(
+	.clk(clk),
+	.rstn(rstn),
+);
+
+initial
+begin
+	#100000000;
+	$finish;
+end
+
+initial
+begin
+	$fsdbDumpfile("./test.fsdb");
+	$fsdbDumpvars(0, tb, "+struct", "+mda");
+	$fsdbDumpMDA(0);
+	$fsdbDumpon;
+end
+
+endmodule
